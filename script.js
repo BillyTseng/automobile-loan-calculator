4bbc633ed4f0430722b06d9c1cb6b083359f6062
Billy Tseng <tseng.billy@gmail.com>
2017-10-06 08:16:07 -0700

    feat: Add the cards of loan option.

--- script.js
+++ script.js
@@ -56,30 +56,102 @@ function isBadInput(element) {
   return ret;
 }
 
+function validateInput(element) {
+  var badInput = isBadInput(element);
+
+  if (badInput) {
+    element.style.borderColor = 'red';
+    element.style.borderWidth = '2px';
+    element.nextElementSibling.style.display = "inline";
+  } else {
+    element.style.borderColor = 'black';
+    element.style.borderWidth = '1px';
+    element.nextElementSibling.style.display = "none";
+  }
+  return badInput;
+}
+
 function validateElement(element) {
   return function() {
-    var badInput = isBadInput(element);
-
-    if (badInput) {
-      element.style.borderColor = 'red';
-      element.style.borderWidth = '2px';
-      element.nextElementSibling.style.display = "inline";
-    } else {
-      element.style.borderColor = 'black';
-      element.style.borderWidth = '1px';
-      element.nextElementSibling.style.display = "none";
-    }
+    validateInput(element);
   };
 }
 
+function removeOption(index) {
+  return function() {
+    this.parentNode.parentNode.removeChild(this.parentNode);
+    optionsArray.splice(index, 1);
+    renderOptions();
+  };
+}
+
+function calPaymentAmount(i) {
+  var p = optionsArray[i].purchasePrice;
+  var r = optionsArray[i].intRate / 100 / 12;
+  var n = optionsArray[i].term;
+  return (p * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+}
+
+function renderOptions() {
+  var optionsElem = document.getElementsByClassName('options')[0];
+  optionsElem.innerHTML = ''; // Clear the privious options.
+  for (var i = 0; i < optionsArray.length; i++) {
+    var htmlStr = '';
+    var loanCardElem = document.createElement('div');
+    loanCardElem.className = 'loanCard';
+
+    var termInMonth = optionsArray[i].term;
+    var monthPay = calPaymentAmount(i);
+    var totalCost = monthPay * termInMonth - optionsArray[i].cashIncentive;
+    monthPay = totalCost / termInMonth;
+    htmlStr += '<p>Offer ' + (i+1) + ':</p>';
+    htmlStr += '<span class="close">x</span>'
+    htmlStr += '<p>Interest Rate: ' + optionsArray[i].intRate.toFixed(2) + '%</p>';
+    htmlStr += '<p>Loan Term (months): ' + optionsArray[i].term + '</p>';
+    htmlStr += '<p>Monthly Payment: $' + monthPay.toFixed(2) + '</p>';
+    htmlStr += '<p>Total cost of Ownership: $' + totalCost.toFixed(2) + '</p>';
+    loanCardElem.innerHTML = htmlStr;
+    optionsElem.appendChild(loanCardElem);
+    document.getElementsByClassName('close')[i].addEventListener("click", removeOption(i));
+  }
+}
+
 function optionView() {
-  console.log("optionView");
+  var inputObj = {};
+  var badInput = false;
+  for(var i=0; i < inputElements.length; i++) {
+      if (inputElements[i].type == "text") {
+          badInput |= validateInput(inputElements[i]);
+      }
+  }
+
+  // If any input element is invalid, return so the table is not udpated.
+  if (badInput) {
+     return;
+  }
+  
+  inputObj.purchasePrice = parseFloat(purchasePriceElem.value);
+  inputObj.cashIncentive = parseFloat(cashIncentiveElem.value);
+  inputObj.intRate = parseFloat(intRateElem.value);
+  inputObj.term = parseFloat(termElem.value);
+
+  optionsArray.push(inputObj);
+  if (optionsArray.length > 3)
+    optionsArray.shift();
+
+  renderOptions();
+  /*  //Debugging
+  for (var i = 0; i < optionsArray.length; i ++) {
+    console.log("optionView: " + Object.values(optionsArray[i]));
+  }*/
 };
 
+var optionsArray = [];
 var purchasePriceElem = document.getElementById('purchasePrice');
 var cashIncentiveElem = document.getElementById('cashIncentive');
 var intRateElem = document.getElementById('intRate');
 var termElem = document.getElementById('term');
+
 var inputElements = document.getElementsByTagName("input");
 
 for(var i=0; i < inputElements.length; i++) {
